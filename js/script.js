da1ca5ed151f6065458c1b2ffa4df15a46e814bb
Abitov Maksud <maksqwerty012@gmail.com>
2022-12-20 11:17:16 +0400

    say my name

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const my_name = [
+    ["Banana", "Orange", "Apple", "Mango"],
+    ["Toyota", "Mitsubishi", "Kia", "Hyundai"],
+    ["Lion","Elephant","Zebra","Blackbird"]
+]
+
+console.log("My name is: " + my_name[1][1][0]+my_name[0][1][2]+my_name[2][3][4]+my_name[1][1][3]+my_name[1][3][2]+my_name[2][3][8])
